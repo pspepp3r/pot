f63e41981e4eee5978cc19aba3308416c78b8f68
pspepp3r <prosperpepple12@gmail.com>
2026-04-07 17:03:01 +0100

    fix: Update project links and improve descriptions; comment out contact form

--- data/projects.js
+++ data/projects.js
@@ -5,7 +5,7 @@ export default [
 		"title": "Bongist",
 		"desc": "Contributed the financial features of the Bongist web application.",
 		"tools": ["PHP", "CSS", "Bootstrap", "Javascript"],
-		"link": "https://www.bongistkoncept.com"
+		"link": "https://www.bongistkoncepts.com"
 	},
 	{
 		"id": 2,
@@ -19,15 +19,15 @@ export default [
 		"id": 3,
 		"image": "./img/book-buzz.jpg",
 		"title": "BookBuzz",
-		"desc": "",
-		"tools": ["Laravel", "React", "MongoDB"],
-		"link": "https://www.github.com/pspepp3r/book-buzz"
+		"desc": "The frontend client for the book-buzz api. View the api code here https://www.github.com/pspepp3r/book-buzz",
+		"tools": ["React", "TailwindCSS"],
+		"link": "https://client-book-buzz.onrender.com"
 	},
 	{
 		"id": 3,
 		"image": "./img/php-json.jpg",
 		"title": "PHP-JSON",
-		"desc": "A PHP Library used to peform CRUD operations on JSON files.",
+		"desc": "A PHP Library used to perform CRUD operations on JSON files.",
 		"tools": ["PHP", "PHPUnit"],
 		"link": "https://www.github.com/pspepp3r/php-json"
 	}
